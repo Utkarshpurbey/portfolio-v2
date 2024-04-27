01eb3aac5ef47017abdc508c39322d6f78c89ea8
Utkarsh <utkarshp@Ninja-ITs-MacBook-Air.local>
2024-04-27 08:39:47 +0530

    react-syntax highlighter added

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,5 +1,28 @@
-const About = () => {
-    return <h1>Hello</h1>
-}
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { codeStyle } from "../utils/utils";
 
-export default About
+//
+const About = () => {
+  const jsxString = ` // <div>
+    //   <h1>Hello this Utkarsh</h1>
+    //   {["a", "b", "c"].map((item, index) => (
+    //     <div key={index}>{item}</div>
+    //   ))}
+    // </div>`;
+  
+  return (
+    <div>
+      <h1>Hello</h1>
+
+      <SyntaxHighlighter
+        language="javascript"
+        style={codeStyle}
+        showLineNumbers={true}
+      >
+        {jsxString}
+      </SyntaxHighlighter>
+    </div>
+  );
+};
+
+export default About;
